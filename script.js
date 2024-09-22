6a4ad2089454c304ee6aad5cbdd9216de583e0b7
Adarsh Mishra <112161648+TheDarkShadow18@users.noreply.github.com>
2024-09-22 15:38:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,11 +82,11 @@ function waitAndResponce(inputText) {
         //     sendTextMessage(resumeString);
         //     break;
         case "skills":
-            sendTextMessage("<span class='sk'>I am currently pursuing MCA.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>ReactJS<br>C<br>C++<br>Javascript<br>SQL<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks :<span class='bold'><br>ReactJs</span><br><br>Favourite IDE:VSCode</span>");
+            sendTextMessage("<span class='sk'>I am currently pursuing MCA.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>C<br>C++<br>MYSQL<br>HTML<br>CSS<br>Javascript</span><br><br>I've experiance with following frameworks :<span class='bold'><br>ReactJs</span><br><br>Favourite IDE:VSCode</span>");
             break;
 
         case "education":
-            sendTextMessage("I am currently pusuing MCA from ABES Engineering College<br>Passing Year : 2025<br><br>I have completed BCA from Deen Dayal Upadhyaya Gorakhpur University<br>Passing Year:2023<br>Result:78.33%<br><br>I have completed my Intermediate from Central Public academy<br>Passing Year:2019");
+            sendTextMessage("I am currently pusuing MCA from ABES Engineering College<br>Passing Year : 2025<br>Result:8.65 SGPA<br><br>I have completed BCA from Deen Dayal Upadhyaya Gorakhpur University<br>Passing Year:2023<br>Result:78.33%<br><br>I have completed my Intermediate from Central Public academy<br>Passing Year:2019<br>Result:73%");
             break;
 
         case "address":
